9f99dc4de8ae6e2a34453fcc6a792962424631d7
VISHNU GUPTA <141792691+Vishnugupta2711@users.noreply.github.com>
2026-07-06 11:58:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1){
-            return n ;
+        if (n <= 1) return n;
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return fib(n-1)+fib(n-2);
+        return dp[n];
     }
 };
+
+// top-down apporach 
+// class Solution {
+// public:
+//     int help(int n , vector<int> &dp){
+//         if(n <=1){
+//             return n;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+//         dp[n] = help(n-1,dp) + help(n-2,dp);
+//         return dp[n];
+//     }
+//     int fib(int n) {
+//         vector<int> dp(n+1,-1);
+//         return help(n,dp);
+//     }
+// };
