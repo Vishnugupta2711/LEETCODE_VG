0065d189464e3ceed15c09034fbb569f0262cd9d
VISHNU GUPTA <141792691+Vishnugupta2711@users.noreply.github.com>
2026-07-07 10:25:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
+    int find(int idx ,vector<int>& nums,int n,vector<int>& dp){
+        if(idx >= n) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        return dp[idx] = max((nums[idx] +find(idx+2,nums,n,dp)),find(idx+1,nums,n,dp));
+    }
     int rob(vector<int>& nums) {
-        int a = 0, b = 0;
-
-        for (int money : nums) {
-            int c = max(b, a + money);
-            a = b;
-            b = c;
-        }
-
-        return b;
+        int n = nums.size();
+        vector<int> dp(n+2,-1);
+        return find(0,nums,n,dp);
     }
 };
