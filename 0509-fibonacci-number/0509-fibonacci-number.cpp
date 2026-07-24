e55a8dbce2a029ba32ff431091ec34496b0bb30b
VISHNU GUPTA <141792691+Vishnugupta2711@users.noreply.github.com>
2026-07-24 22:36:35 +0530

    Time: 2 ms (77.96%), Space: 7.7 MB (65.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1){
-            return n;
+        if(n <= 1) return n;
+        int prev2 = 0 ;
+        int prev1 = 1;
+        int curr;
+        for(int i = 2 ; i <= n ;i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        int first = 0;
-        int second = 1;
-        int res ;
-        for(int i = 2 ;i<=n;i++){
-            res = first + second;
-            first = second ;
-            second = res;
-        }
-        return res;
+        return prev1;
     }
 };
 
+
+
+// space optimisation 
 
 
 
