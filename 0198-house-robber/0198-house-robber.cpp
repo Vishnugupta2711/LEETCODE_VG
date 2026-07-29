bca7a9430c3949cabe6c8d4ddd125479ce9ca435
VISHNU GUPTA <141792691+Vishnugupta2711@users.noreply.github.com>
2026-07-29 22:32:59 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,53 @@
-// space-optimization
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        int last = 0;
-        int seclast = 0;
-        int curr;
-        for(int i = n-1 ;i >=0;i--){
-            curr = max(nums[i]+last , seclast);
-            last = seclast;
-            seclast = curr;
-        }
-        return seclast;
+       int n = nums.size();
+       int next1 = 0 ; 
+       int next2 = 0;
+       int res ;
+       for(int i = n-1;i>=0;i--){
+            res = max(nums[i] + next2, next1);
+            next2 = next1;
+            next1 = res;
+       }
+       return next1;
     }
 };
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n+2,0);
+//         for(int i = n-1 ; i >=0;i--){
+//             int take = nums[i] + dp[i+2];
+//             int skip = dp[i+1];
+//             dp[i] = max(take,skip);
+//         }
+//         return dp[0];
+//     }
+// };
+
+
+
+
+
+// space-optimization
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         int last = 0;
+//         int seclast = 0;
+//         int curr;
+//         for(int i = n-1 ;i >=0;i--){
+//             curr = max(nums[i]+last , seclast);
+//             last = seclast;
+//             seclast = curr;
+//         }
+//         return seclast;
+//     }
+// };
 
 
 
